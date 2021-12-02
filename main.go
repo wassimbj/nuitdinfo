b3e14970a14643353e15b9e38f9fd14548c4ae21
wassim <wassimbenjdida@gmail.com>
2021-12-02 18:12:19 +0100

    removed bad comments

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"nuitdinfo.api/config"
 )
 
 func main() {
-	app := fiber.New() //? app = express()
+	app := fiber.New()
 
 	//? setup cors
 	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		log.Println(config.GetEnv("MYSQL_ROOT_PASSWORD"))
 		return c.SendString("Hello 👋!")
 	})
 
